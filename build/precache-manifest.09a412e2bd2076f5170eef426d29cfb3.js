72232de34a58a97308b3457cebc38f63ea380cda
swetakarar <kararsweta@gmail.com>
2021-09-06 21:35:08 +0530

    firebase data server fixed

---
+++ build/precache-manifest.09a412e2bd2076f5170eef426d29cfb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580e87c7ee89cb4e2f925cad4714d957",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "c12f0e188f508926a6e9",
+    "url": "/chatapp/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "27399018316b2e7982ef",
+    "url": "/chatapp/static/js/2.7cbb55ff.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/chatapp/static/js/2.7cbb55ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12f0e188f508926a6e9",
+    "url": "/chatapp/static/js/main.b66c7bba.chunk.js"
+  },
+  {
+    "revision": "84408a144bef143a2e52",
+    "url": "/chatapp/static/js/runtime-main.ceabeb6b.js"
+  }
+]);
